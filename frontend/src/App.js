80008cd8301690901bb0be4e07f812633c6a20e1
Clucas0311 <clucas0311@gmail.com>
2021-02-21 19:34:21 -0500

    Modified Font and Header And Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ const App = () => {
 	return (
 		<>
 			<Header />
-			<main>
+			<main className="py-3">
 				<Container>
-					<h1>Welcome To The Proshop</h1>
+					<h1>Welcome To The ProShop</h1>
 				</Container>
-				<Footer />
 			</main>
+			<Footer />
 		</>
 	);
 };
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<main>
+						<Col className="text-center py-3">Copyright &copy; ProShop</Col>
+					</main>
+				</Row>
+			</Container>
+		</footer>
+	);
 };
 
 export default Footer;
