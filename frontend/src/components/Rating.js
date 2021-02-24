88ac66912dca2de28ff6675e81d8e3f60a90b768
Clucas0311 <clucas0311@gmail.com>
2021-02-23 20:18:15 -0500

    Feature - Single Item Feature

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -45,7 +45,7 @@ Rating.defaultProps = {
 
 Rating.propTypes = {
 	value: PropTypes.number.isRequired,
-	string: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
 	color: PropTypes.string
 };
 export default Rating;
